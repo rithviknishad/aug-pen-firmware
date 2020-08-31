d524fe011ae5429ca64ad7455145253e87c69cec
Rithvik Nishad <rithvikn2001@gmail.com>
2020-08-31 18:58:23 +0530

    Import Libraries

--- aug-pen-firmware.ino
+++ aug-pen-firmware.ino
@@ -1,52 +1,58 @@
+#include <ESP8266WiFi.h>
 #include <Wire.h>
 #include <PubSubClient.h>
-#include <ESP8266WiFi.h>
+#include <MPU6050_6Axis_MotionApps20.h>
 
 #define X     0
 #define Y     1
 #define Z     2
-#define ALPHA X
-#define BETA  Y
-#define GAMMA Z
 
 /* 
-  ESP8266 PIN MAP
-  ===============
+  SCHEMATIC
 
-  GND     -->     GND
-  GPIO2   -->     MPU6050_SCL
-  GPIO0   -->     MPU6050_SDA   (programming mode: LOW before startup)
-  RX      -->     
-  TX      -->     
-  CH_EN   -->     VCC
-  RESET   -->     
-  VCC     -->     VCC
-
+  ESP8266     MPU-6050    FLASH
+ --------------------------------
+  GND         --------GND--------
+  GPIO2       SCL         _GPIO2
+  GPIO0       SDA         _GPIO1
+  RX          INT         _RX
+  TX          --------_TX--------
+  CH_EN       --------VCC--------
+  RST         --------RST--------
+  VCC         --------VCC--------
 */
 
-char buff[100];
+char buff[100]; // A globally shared 100 byte buffer space.
 
-const int MPU = 0x68; // I2C Address of MPU-6050 (default)
+const int MPU = 0x68; // I2C Address of MPU-6050 (default).
 
 // WiFi Connection Configuration
-const char *wifi_ssid = "Honor 8C";
-const char *wifi_password = "16june01";
+const char *wifi_ssid = "Honor 8C";     // SSID of the WiFi AP to be connected.
+const char *wifi_password = "16june01"; // Password of the WiFi AP.
 
 // MQTT Connection Configuration
-const char *mqtt_server = "192.168.43.20";
-const char *mqtt_username = "user";
-const char *mqtt_password = "iL0v3MoonGaYoung";
+const char *mqtt_server = "192.168.43.20";  // MQTT Broker Server Address.
+const char *mqtt_username = "user"; // MQTT Credentials - Username.
+const char *mqtt_password = "iL0v3MoonGaYoung"; // MQTT Credentials - Password.
 
-WiFiClient wifi;
-PubSubClient mqtt(wifi);
+WiFiClient wifi;  // WiFi Client for MQTT Client.
+PubSubClient mqtt(wifi);  // MQTT PubSub Client.
 
-float ACCEL[] = { 0, 0, 0 };
-float GYRO[] = { 0, 0, 0 };
+float ACCEL[3] = {0};  // Current raw acceleration samples from MPU-6050.
+float GYRO[3]  = {0};  // Current raw gyroscope samples from MPU-6050.
+
+float VELOCITY[3] = {0};
+float ANGULAR_VELOCITY[3] = {0};
+
+float POSITION[3]     = {0};  // Current position, double integrated from ACCEL.
+float ORIENTATION[3]  = {0};  // Current orientation, double integrated from GYRO.
 
 void initMPU6050();
 void initWiFi();
 void initMQTT();
 bool reconnectMQTT();
+inline unsigned long square(long x) { return x * x; }
+inline float squaref(float x) { return x * x; }
 void updateSamples();
 void debugSamples();
 void publishSamples();
@@ -58,7 +64,7 @@ void setup() {
   initWiFi();
   initMQTT();
 
-  Serial.println("AUG PEN MK I : boot success.");
+  Serial.println(F("AUG PEN MK I : boot success."));
   delay(2000);
 }
 
@@ -88,10 +94,10 @@ void initWiFi() {
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    Serial.print('.');
   }
 
-  Serial.print("Connected. What's my IP? ");
+  Serial.print(F("Connected. What's my IP? "));
   Serial.println(WiFi.localIP());
 }
 
@@ -101,45 +107,56 @@ void initMQTT() {
 }
 
 bool reconnectMQTT() {
-  Serial.print("MQTT Reconnecting");
+  Serial.print(F("MQTT Reconnecting"));
 
   while (!mqtt.connected()) {
     if (!mqtt.connect("AUG_PEN_MK_I", mqtt_username, mqtt_password)) {
-      Serial.println("MQTT connection unsuccessfull.");
+      Serial.println(F("MQTT connection unsuccessfull."));
       Serial.println(mqtt.state());
-      Serial.println("Trying again in 3 seconds...");
+      Serial.println(F("Trying again in 3 seconds..."));
       delay(3000);
     } else {
-      Serial.println("MQTT connection successfull.");
+      Serial.println(F("MQTT connection successfull."));
     }
   }
 
-  Serial.println("Connected to MQTT Server.");
+  Serial.println(F("Connected to MQTT Server."));
 
   return mqtt.connected();
 }
+
+float t0 = 0;
 
 void updateSamples() {
   Wire.beginTransmission(MPU);
   Wire.write(0x3B); // ACCEL_XOUT_H Register
   Wire.endTransmission(false);
   Wire.requestFrom(MPU, 14, true); // Request 14 bytes
-  ACCEL[X]    = ((int) (Wire.read() << 8) | Wire.read()) / 4096.0;       
-  ACCEL[Y]    = ((int) (Wire.read() << 8) | Wire.read()) / 4096.0;
-  ACCEL[Z]    = ((int) (Wire.read() << 8) | Wire.read()) / 4096.0;  
-  GYRO[ALPHA] = ((int) (Wire.read() << 8) | Wire.read()) / 32.8;  
-  GYRO[BETA]  = ((int) (Wire.read() << 8) | Wire.read()) / 32.8;  
-  GYRO[GAMMA] = ((int) (Wire.read() << 8) | Wire.read()) / 32.8; 
+
+  for (int i = 0; i < 3; ++i)
+    ACCEL[i] = (Wire.read() << 8) | Wire.read();
+
+  int temp = (Wire.read() << 8) | Wire.read();
+
+  for (int i = 0; i < 3; ++i)
+    GYRO[i] = (Wire.read() << 8) | Wire.read();
+
+  float delta = (millis() / 1000) - t0;
+  float delta_square = squaref(delta);
+
+  
+
+  t0 += delta;
 }
 
 void debugSamples() {
-  Serial.print("ACX=");   Serial.print(ACCEL[X]);
-  Serial.print("\tACY="); Serial.print(ACCEL[Y]);
-  Serial.print("\tACZ");  Serial.print(ACCEL[Z]);
+  Serial.print(F("\tpX="));  Serial.print(POSITION[X]);
+  Serial.print(F("\tpY="));  Serial.print(POSITION[Y]);
+  Serial.print(F("\tpZ="));   Serial.print(POSITION[Z]);
 
-  Serial.print("\tGYX="); Serial.print(GYRO[X]);
-  Serial.print("\tGYY="); Serial.print(GYRO[Y]);
-  Serial.print("\tGYZ="); Serial.print(GYRO[Z]);
+  Serial.print(F("\toX="));  Serial.print(ORIENTATION[X]);
+  Serial.print(F("\toY="));  Serial.print(ORIENTATION[Y]);
+  Serial.print(F("\toZ="));  Serial.print(ORIENTATION[Z]);
 
   Serial.println();
 }
@@ -150,10 +167,11 @@ char* floatToString(const float value) {
 }
 
 void publishSamples() {
-  mqtt.publish("ACX/", floatToString(ACCEL[X]));
-  mqtt.publish("ACY/", floatToString(ACCEL[Y]));
-  mqtt.publish("ACZ/", floatToString(ACCEL[Z]));
-  mqtt.publish("GYX/", floatToString(GYRO[X]));
-  mqtt.publish("GYY/", floatToString(GYRO[Y]));
-  mqtt.publish("GYZ/", floatToString(GYRO[Z]));
+  mqtt.publish("position/X", floatToString(POSITION[X]));
+  mqtt.publish("position/Y", floatToString(POSITION[Y]));
+  mqtt.publish("position/Z", floatToString(POSITION[Z]));
+
+  mqtt.publish("orientation/X", floatToString(ORIENTATION[X]));
+  mqtt.publish("orientation/Y", floatToString(ORIENTATION[Y]));
+  mqtt.publish("orientation/Z", floatToString(ORIENTATION[Z]));
 }
