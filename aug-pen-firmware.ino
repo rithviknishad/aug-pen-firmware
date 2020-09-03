cbd5f079a88f976a6319335cb4ee5df45d6cf46f
Rithvik Nishad <rithvikn2001@gmail.com>
2020-09-03 15:12:25 +0530

    Clean Preprocessor directives

--- aug-pen-firmware.ino
+++ aug-pen-firmware.ino
@@ -4,8 +4,8 @@
 */
 
 // #define RELEASE         // Comment this to compile without including development and diagnostics code for faster performance.
-// #define USE_WIFI_MQTT   // Comment this line to disable WiFi and MQTT. (Allows compatability for use w/ non wifi boards for development and testing)
-// #define TEAPOT_ENABLED  // Comment this line to disable support for Teapot.
+// #define USE_WIFI_MQTT   // Comment this line to disable WiFi and MQTT. (Allows compatability for use w/ non wifi boards for development and testing).
+#define TEAPOT_ENABLED  // DOES NOT WORK IN RELEASE MODE. DISABLE RELEASE. Comment this line to disable support for Teapot.
 
 #include <MPU6050_6Axis_MotionApps20.h> // Library to interact w/ MPU6050's DMP engine to offload computation and reduce drift over time in integrated samplings.
 // The above library defines: _MPU6050_6AXIS_MOTIONAPPS20_H_ as header guard. Can be used to detect which API is being used for development.
@@ -41,81 +41,80 @@
 #define MPU6050_I2C_ADDRESS 0x68
 
 #ifndef RELEASE
-  #define SERIAL_ENABLED       // Comment to disable Serial communication. INFO and DEBUGGING will also be disabled.
-  #define POST_BOOT_DELAY 2000 // Explicit Post Boot Delay in milli-seconds.
-#endif
-
-#ifdef SERIAL_ENABLED
+  #define SERIAL_ENABLED 115200       // Comment to disable Serial communication. INFO and DEBUGGING will also be disabled. Also defines the baud rate.
+  
   #define INFO_ENABLED    // Comment to disable INFO messages. (disabling allows working w/ Serial Plotter)
   #define ASSERTS_ENABLED // Comment to disable ASSERT messages.
   #define DEBUG_ENABLED   // Comment to disable DEBUG messages.
-  #define SERIAL_PARAM(name, value)                                                                        \
-    Serial.print('\t');                                                                                    \
-    Serial.print(F(name));                                                                                 \
-    Serial.print(value);
+  
+  #define POST_BOOT_DELAY 2000 // Explicit Post Boot Delay in milli-seconds.
 #endif
+
+#define GET_VARIABLE_NAME(var) (#var)
+
+#ifdef SERIAL_ENABLED
+  #define SERIAL_OUT_CONST(msg) Serial.print(F(msg));
+  #define SERIAL_OUT(var) Serial.print(var);
+  #define SERIAL_OUT_LN Serial.println();
+#else
+  #define SERIAL_OUT_CONST(msg)
+  #define SERIAL_OUT(var)
+  #define SERIAL_OUT_LN
+#endif
+
+#define SERIAL_OUT_PARAM_MANUAL(name, value) SERIAL_OUT_CONST(name) SERIAL_OUT_CONST(": ") SERIAL_OUT(value)
+#define SERIAL_OUT_PARAM_AUTO(param) SERIAL_OUT_PARAM_MANUAL(GET_VARIABLE_NAME(param), param);
+
+#define GET_SERIAL_OUT_PARAM_MACRO(_1, _2, NAME, ...) NAME
+#define SERIAL_OUT_PARAM(...) GET_SERIAL_OUT_PARAM_MACRO(__VA_ARGS__, SERIAL_OUT_PARAM_MANUAL, SERIAL_OUT_PARAM_MANUAL) (__VA_ARGS__)
 
 #ifdef INFO_ENABLED
-  #define INFO_PARAM(name, value) SERIAL_PARAM(name, value)
-  #define INFO(x) Serial.println(F(x)); // Use INFO("...") to Serial LOG a compile-time constant string.
-#else
-  #define INFO_PARAM(name, val) ;
-  #define INFO(x) ;
-#endif
-
-#ifdef ASSERTS_ENABLED
-bool result;
-#define ASSERT(x, msg)                  \
-  if (!(result = x))                    \
-  {                                     \
-    Serial.println(F(msg));             \
-    Serial.print(F("ASSERT_RESULT: ")); \
-    Serial.print(result);               \
-  }
-#else
-  #define ASSERT(x, msg) ;
+  #define INFO(x) SERIAL_OUT_CONST(x) // Use INFO("...") to Serial LOG a compile-time constant string.
+  #define INFO_PARAM(...) SERIAL_OUT_PARAM(...)
 #endif
 
 #ifdef DEBUG_ENABLED
-  #define DEBUG(param)   \
-    Serial.print(param); \
-    Serial.print('\t');
-  #define DEBUG_PARAM(name, value) SERIAL_PARAM(name, value)
-  #define END_DEBUG Serial.println();
-#else
-  #define DEBUG(param) ;
-  #define DEBUG_PARAM(name, value) ;
-  #define END_DEBUG ;
+  #define DEBUG(...) SERIAL_OUT_PARAM(...)
+  #define END_DEBUG SERIAL_OUT_LN
+#endif
+
+#ifdef ASSERTS_ENABLED
+  bool result;
+
+  #define BLOCK_ON_ASSERT_FAIL true // Set to false to disable blocking on ASSERT fail.
+  #define ASSERT(x, msg)                     \
+    if (!(result = x))                       \
+    {                                        \
+      SERIAL_OUT_CONST(msg)                  \
+      SERIAL_OUT_PARAM("ASSERT_FAILED", x)   \
+      while (BLOCK_ON_ASSERT_FAIL) ;         \
+    }
 #endif
 
 MPU6050 mpu = MPU6050(MPU6050_I2C_ADDRESS);
 
-uint8_t dmpReady = false;   // DMP ready status.
 uint8_t mpuIntStatus;       // Current interrupt status byte.
-uint8_t devStatus;          // Status after each operation. [0 = success, !0 = error]
 uint16_t packetSize;        // Expected DMP packet size (42 btes default)
 uint16_t fifoCount;         // count of all bytes currently in FIFO.
 uint8_t fifoBuffer[64];     // FIFO storage buffer
 
 volatile bool mpuInterrupt = false;
-void dmpDataReady() { mpuInterrupt = true; }
+void dmpDataReady() { mpuInterrupt = true; } // Interrupt Service Routine when DMP data is ready.
 
 Quaternion q;        // [w, x, y, z] MPU6050 DMP quaternion container.
 VectorInt16 aa;      // [x, y, z] ACCEL_RAW.
 VectorInt16 aaReal;  // [x, y, z] Gravity free ACCEL.
 VectorInt16 aaWorld; // [x, y, z] World frame ACCEL.
 VectorFloat gravity;  // [x, y, z] Gravity vector.
-float euler[3];      // [psi, theta, phi] Euler angles.
+// float euler[3];      // [psi, theta, phi] Euler angles.
 float ypr[3];        // [yaw, pitch, roll] angles.
 
-char buff[100]; 
-
-#ifdef TEAPOT_ENABLED
-  #define SERIAL_ENABLED
+#if defined(TEAPOT_ENABLED) && defined(SERIAL_ENABLED)
   uint8_t teapotPacket[14] = {'$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n'};
 #endif
 
 #ifdef USE_WIFI_MQTT
+  char buff[50]; 
   // WiFi Connection Configuration
   const char *wifi_ssid = "Honor 8C";     // SSID of the WiFi AP to be connected.
   const char *wifi_password = "16june01"; // Password of the WiFi AP.
@@ -129,50 +128,10 @@ char buff[100];
   PubSubClient mqtt(wifi);  // MQTT PubSub Client.
 #endif
 
-#define X     0
-#define Y     1
-#define Z     2
-#define ROLL  X
-#define YAW   Y
-#define PITCH Z
+#define DEBUG_VECTOR(v) SERIAL_OUT_CONST(GET_VARIABLE_NAME(v)) SERIAL_OUT_CONST(" = [") SERIAL_OUT(v.x) SERIAL_OUT_CONST(", ") SERIAL_OUT(v.y) SERIAL_OUT_CONST(", ") SERIAL_OUT(v.z) SERIAL_OUT_CONST("]\t")
+#define DEBUG_ARRAY(a) SERIAL_OUT_CONST(GET_VARIABLE_NAME(a)) SERIAL_OUT_CONST(" = [") SERIAL_OUT(a[0]) SERIAL_OUT_CONST(", ") SERIAL_OUT(a[1]) SERIAL_OUT_CONST(", ") SERIAL_OUT(a[2]) SERIAL_OUT_CONST("]\t")
 
-// float ACCEL[3] = {0};  // Current raw acceleration samples from MPU-6050.
-// float GYRO[3]  = {0};  // Current raw gyroscope samples from MPU-6050.
-
-// float VELOCITY[3] = {0}; // Current velocity, integrated from ACCEL.
-
-// float POSITION[3]     = {0};  // Current position, integrated from VELOCITY.
-// float ORIENTATION[3]  = {0};  // Current orientation, integrated from GYRO.
-
-#define GET_VARIABLE_NAME(Variable) (#Variable)
-
-#define DEBUG_VECTOR(v)                  \
-  Serial.print(F(GET_VARIABLE_NAME(v))); \
-  Serial.print(F(" = ["));               \
-  Serial.print(v.x);                     \
-  Serial.print(", ");                    \
-  Serial.print(v.y);                     \
-  Serial.print(", ");                    \
-  Serial.print(v.z);                     \
-  Serial.print(F("]\t"));
-
-#define DEBUG_ARRAY(arr)                   \
-  Serial.print(F(GET_VARIABLE_NAME(arr))); \
-  Serial.print(F(" = ["));                 \
-  Serial.print(arr[X]);                    \
-  Serial.print(", ");                      \
-  Serial.print(arr[Y]);                    \
-  Serial.print(", ");                      \
-  Serial.print(arr[Z]);                    \
-  Serial.print(F("]\t"));
-
-#define DEBUG_SAMPLES    \
-  DEBUG_VECTOR(aaWorld); \
-  DEBUG_ARRAY(ypr);      \
-  END_DEBUG;
-  // DEBUG_VECTOR("VX=", "VY=", "VZ=", VELOCITY);    \
-  // DEBUG_VECTOR("PX=", "PY=", "PZ=", POSITION);    \
-  // DEBUG_VECTOR("OX=", "OY=", "OZ=", ORIENTATION); \
+#define DEBUG_SAMPLES DEBUG_VECTOR(aaWorld) DEBUG_ARRAY(ypr) SERIAL_OUT_LN
 
 void initMPU6050(); // Initialises MPU-6050.
 
@@ -187,7 +146,7 @@ void updateSamples(); // Get and update the samples
 
 void setup() {
   #ifdef SERIAL_ENABLED
-    Serial.begin(2000000);
+    Serial.begin(SERIAL_ENABLED);
   #endif
 
   initMPU6050();
@@ -195,9 +154,7 @@ void setup() {
   #ifdef USE_WIFI_MQTT
     initWiFi();
     initMQTT();
-  #endif
 
-  #ifdef USE_WIFI_MQTT
     INFO("AUG PEN MK I : boot_result=0x0, success.");
   #else
     INFO("AUG PEN MK I : boot_result=0x1, success.");
@@ -210,10 +167,6 @@ void setup() {
 }
 
 void loop() {
-
-  if (!dmpReady)
-    return; // skip loop if dmp failed to load.
-
   updateSamples();
   DEBUG_SAMPLES;
 
@@ -221,7 +174,6 @@ void loop() {
     mqtt.connected()
         ? mqtt.loop()
         : reconnectMQTT();
-
     publishSamples();
   #endif
 }
@@ -242,8 +194,7 @@ void initMPU6050() {
   pinMode(MPU_INTERRUPT_PIN, INPUT);
 
   INFO("Initializing Digital Motion Processing Engine...");
-  devStatus = mpu.dmpInitialize();
-  ASSERT(devStatus == 0, "DMP Initialization failed.")
+  ASSERT(mpu.dmpInitialize() == 0, "DMP Initialization failed.") //  { 0: success, !0: fail }
 
   INFO("Setting custom offsets...");
   // mpu.setXGyroOffset(220);
@@ -265,7 +216,6 @@ void initMPU6050() {
   packetSize = mpu.dmpGetFIFOPacketSize();
 
   INFO("DMP Engine is ready.");
-  dmpReady = true;
 }
 
 #ifdef USE_WIFI_MQTT
@@ -277,7 +227,7 @@ void initMPU6050() {
     while (WiFi.status() != WL_CONNECTED) {
       delay(500);
   #ifdef INFO_ENABLED
-      Serial.print('.');
+      SERIAL_OUT_CONST('.');
   #endif
     }
     INFO_PARAM("WiFi Connected. What's my IP? ", WiFi.localIP());
