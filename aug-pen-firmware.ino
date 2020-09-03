159d2cf37fb417a7fbbef57aeb5c726c2f141c80
Rithvik Nishad <rithvikn2001@gmail.com>
2020-09-03 08:36:41 +0530

    Strip clean, remove non-dmp motion_apps code

--- aug-pen-firmware.ino
+++ aug-pen-firmware.ino
@@ -17,32 +17,6 @@
   #include <PubSubClient.h>
 #endif
 
-#ifndef _MPU6050_6AXIS_MOTIONAPPS20_H_
-
-  /*
-    AFS_SEL   FULL SCALE RANGE    LSB SENSITIVITY
-    ---------------------------------------------
-      0             ±2g            16384 LSB/g
-      1             ±4g            8192  LSB/g
-      2             ±8g            4096  LSB/g
-      3             ±16g           2048  LSB/g
-  */
-  #define AFS_SEL 0
-  #define ACCEL_SENSITIVITY (16384.0 / pow(2, AFS_SEL)) // LSB/g
-
-  /*
-    FS_SEL    FULL SCALE RANGE    LSB SENSITIVITY
-    ---------------------------------------------
-      0         ± 250 °/s           131  LSB/°/s
-      1         ± 500 °/s           65.5 LSB/°/s
-      2         ± 1000 °/s          32.8 LSB/°/s 
-      3         ± 2000 °/s          16.4 LSB/°/s
-  */
-  #define FS_SEL 0
-  #define GYRO_SENSITIVITY  (131.0 / pow(2, FS_SEL)) // LSB/°/s
-
-#endif
-
 /* 
   SCHEMATIC
 
@@ -57,13 +31,12 @@
   _GPIO2    GPIO2       SCL         A5
   GND       GND         GND         GND
 */
-#ifdef _MPU6050_6AXIS_MOTIONAPPS20_H_
-  #ifdef ESP8266
-    #define MPU_INTERRUPT_PIN 3
-  #else
-    #define MPU_INTERRUPT_PIN 2
-  #endif;
-#endif
+
+#ifdef ESP8266
+  #define MPU_INTERRUPT_PIN 3
+#else
+  #define MPU_INTERRUPT_PIN 2
+#endif;
 
 #define MPU6050_I2C_ADDRESS 0x68
 
@@ -115,37 +88,27 @@ bool result;
   #define END_DEBUG ;
 #endif
 
-#ifdef _MPU6050_6AXIS_MOTIONAPPS20_H_
-  MPU6050 mpu = MPU6050(MPU6050_I2C_ADDRESS);
+MPU6050 mpu = MPU6050(MPU6050_I2C_ADDRESS);
 
-  uint8_t dmpReady = false;   // DMP ready status.
-  uint8_t mpuIntStatus;       // Current interrupt status byte.
-  uint8_t devStatus;          // Status after each operation. [0 = success, !0 = error]
-  uint16_t packetSize;        // Expected DMP packet size (42 btes default)
-  uint16_t fifoCount;         // count of all bytes currently in FIFO.
-  uint8_t fifoBuffer[64];     // FIFO storage buffer
+uint8_t dmpReady = false;   // DMP ready status.
+uint8_t mpuIntStatus;       // Current interrupt status byte.
+uint8_t devStatus;          // Status after each operation. [0 = success, !0 = error]
+uint16_t packetSize;        // Expected DMP packet size (42 btes default)
+uint16_t fifoCount;         // count of all bytes currently in FIFO.
+uint8_t fifoBuffer[64];     // FIFO storage buffer
 
-  volatile bool mpuInterrupt = false;
-  void dmpDataReady() { mpuInterrupt = true; }
+volatile bool mpuInterrupt = false;
+void dmpDataReady() { mpuInterrupt = true; }
 
-  Quaternion q;        // [w, x, y, z] MPU6050 DMP quaternion container.
-  VectorInt16 aa;      // [x, y, z] ACCEL_RAW.
-  VectorInt16 aaReal;  // [x, y, z] Gravity free ACCEL.
-  VectorInt16 aaWorld; // [x, y, z] World frame ACCEL.
-  VectorFloat gravity;  // [x, y, z] Gravity vector.
-  float euler[3];      // [psi, theta, phi] Euler angles.
-  float ypr[3];        // [yaw, pitch, roll] angles.
+Quaternion q;        // [w, x, y, z] MPU6050 DMP quaternion container.
+VectorInt16 aa;      // [x, y, z] ACCEL_RAW.
+VectorInt16 aaReal;  // [x, y, z] Gravity free ACCEL.
+VectorInt16 aaWorld; // [x, y, z] World frame ACCEL.
+VectorFloat gravity;  // [x, y, z] Gravity vector.
+float euler[3];      // [psi, theta, phi] Euler angles.
+float ypr[3];        // [yaw, pitch, roll] angles.
 
-#else
-
-  const int MPU = MPU6050_I2C_ADDRESS; // I2C Address of MPU-6050 (default).
-  
-  char buff[100];                      // A globally shared 100 byte buffer space.
-  
-  inline unsigned long square(long x) { return x * x; }
-  inline float squaref(float x) { return x * x; }
-
-#endif
+char buff[100]; 
 
 #ifdef TEAPOT_ENABLED
   #define SERIAL_ENABLED
@@ -271,48 +234,38 @@ void initMPU6050() {
     Wire.begin();
   #endif
 
-  #ifdef _MPU6050_6AXIS_MOTIONAPPS20_H_
-    Wire.setClock(400000);
+  Wire.setClock(400000);
 
-    mpu.initialize();
-    ASSERT(mpu.testConnection(), "MPU-6050 connection error !")
+  mpu.initialize();
+  ASSERT(mpu.testConnection(), "MPU-6050 connection error !")
 
-    pinMode(MPU_INTERRUPT_PIN, INPUT);
+  pinMode(MPU_INTERRUPT_PIN, INPUT);
 
-    INFO("Initializing Digital Motion Processing Engine...");
-    devStatus = mpu.dmpInitialize();
-    ASSERT(devStatus == 0, "DMP Initialization failed.")
+  INFO("Initializing Digital Motion Processing Engine...");
+  devStatus = mpu.dmpInitialize();
+  ASSERT(devStatus == 0, "DMP Initialization failed.")
 
-    INFO("Setting custom offsets...");
-    // mpu.setXGyroOffset(220);
-    // mpu.setYGyroOffset(76);
-    // mpu.setZGyroOffset(-85);
-    // mpu.setZAccelOffset(1788);
+  INFO("Setting custom offsets...");
+  // mpu.setXGyroOffset(220);
+  // mpu.setYGyroOffset(76);
+  // mpu.setZGyroOffset(-85);
+  // mpu.setZAccelOffset(1788);
 
-    INFO("Calibrating Accelerometer and Gyroscope...");
-    mpu.CalibrateAccel(12);
-    mpu.CalibrateGyro(12);
+  INFO("Calibrating Accelerometer and Gyroscope...");
+  mpu.CalibrateAccel(12);
+  mpu.CalibrateGyro(12);
 
-    INFO("Enabling DMP Engine...");
-    mpu.setDMPEnabled(true);
+  INFO("Enabling DMP Engine...");
+  mpu.setDMPEnabled(true);
 
-    INFO("Attaching Interrupt Service Routine...");
-    attachInterrupt(digitalPinToInterrupt(MPU_INTERRUPT_PIN), dmpDataReady, RISING);
-    mpuIntStatus = mpu.getIntStatus();
+  INFO("Attaching Interrupt Service Routine...");
+  attachInterrupt(digitalPinToInterrupt(MPU_INTERRUPT_PIN), dmpDataReady, RISING);
+  mpuIntStatus = mpu.getIntStatus();
 
-    packetSize = mpu.dmpGetFIFOPacketSize();
+  packetSize = mpu.dmpGetFIFOPacketSize();
 
-    INFO("DMP Engine is ready.");
-    dmpReady = true;
-
-  #else
-    Wire.beginTransmission(MPU);
-    Wire.write(0x6B); // PWR_MGMT_1 Register
-    Wire.write(0);    // Wake the MPU-6050.
-    Wire.endTransmission(true);
-    
-    INFO("MPU-6050 Connected. Status: Unknown");
-  #endif  
+  INFO("DMP Engine is ready.");
+  dmpReady = true;
 }
 
 #ifdef USE_WIFI_MQTT
@@ -353,104 +306,57 @@ void initMPU6050() {
   
 #endif
 
-#ifdef _MPU6050_6AXIS_MOTIONAPPS20_H_
-
-  void updateSamples() {
-    while(!mpuInterrupt && fifoCount < packetSize) {
-      if (mpuInterrupt && fifoCount < packetSize)
-        fifoCount = mpu.getFIFOCount();
-    }
-
-    mpuInterrupt = false;
-    mpuIntStatus = mpu.getIntStatus();
-
-    fifoCount = mpu.getFIFOCount();
-
-    if (fifoCount < packetSize) {
-      
-    } else if ((mpuIntStatus & (0x01 << MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
-      INFO("FIFO overflow! Resetting...");
-      mpu.resetFIFO();
-    } else if (mpuIntStatus & (0x01 << MPU6050_INTERRUPT_DMP_INT_BIT)) {
-      while (fifoCount >= packetSize) {
-        mpu.getFIFOBytes(fifoBuffer, packetSize);
-        fifoCount -= packetSize;
-      }
-
-      mpu.dmpGetQuaternion(&q, fifoBuffer);
-      mpu.dmpGetAccel(&aa, fifoBuffer);
-      mpu.dmpGetGravity(&gravity, &q);
-
-      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-      mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-      mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-
-      #ifdef TEAPOT_ENABLED
-
-        #define mapBufferToTeapot(tp, fb) teapotPacket[tp] = fifoBuffer[fb];
-
-        mapBufferToTeapot(2, 0);
-        mapBufferToTeapot(3, 1);
-        mapBufferToTeapot(4, 4);
-        mapBufferToTeapot(5, 5);
-        mapBufferToTeapot(6, 8);
-        mapBufferToTeapot(7, 9);
-        mapBufferToTeapot(8, 12);
-        mapBufferToTeapot(9, 13);
-
-        Serial.write(teapotPacket, 14); // flushes teapotPacket to Serial.
-
-        teapotPacket[11]++; // teapot packetCount (overflow loop at 0xFF is intended).
-
-      #endif
-    }
+void updateSamples() {
+  while(!mpuInterrupt && fifoCount < packetSize) {
+    if (mpuInterrupt && fifoCount < packetSize)
+      fifoCount = mpu.getFIFOCount();
   }
 
-#else
-  // stores the last sampling time, for measuring delta time between last two samples.
-  uint32_t sampling_time[2] = {0};
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
 
-  void updateSamples() {
-    // request 14 bytes of data from register 0x3B (ACCEL_XOUT_H).
-    Wire.beginTransmission(MPU);
-    Wire.write(0x3B);
-    Wire.endTransmission(false);
-    Wire.requestFrom(MPU, 14, true);
+  fifoCount = mpu.getFIFOCount();
+
+  if (fifoCount < packetSize) {
     
-    // Read all ACCEL registers (2 * 3 bytes) and store in m/s^2.
-    for (int i = 0; i < 3; ++i)
-      ACCEL[i] = (((Wire.read() << 8) | Wire.read()) / ACCEL_SENSITIVITY) * 9.80655;
-
-    // Read temperature register (2 bytes) and store as degree celsius.
-    float temp = (((int)(Wire.read() << 8) | Wire.read()) / 340.0) + 36.53;
-
-    // Read all GRYO regsters (2 * 3 bytes) and store in deg/s.
-    for (int i = 0; i < 3; ++i)
-      GYRO[i] = ((Wire.read() << 8) | Wire.read()) / GYRO_SENSITIVITY;
-
-    // move previous sampling time.
-    sampling_time[0] = sampling_time[1];
-
-    // fetch current sampling time.
-    sampling_time[1] = millis();
-
-    // check overflow ?? skip this sampling.
-    if (sampling_time[1] < sampling_time[0])
-      return;
-
-    // get sampling time delta in seconds.
-    float delta = (sampling_time[1] - sampling_time[0]) / 1000;
-
-    // compute velocity, position and orientation.
-    for (int i = 0; i < 3; ++i) {
-      VELOCITY[i] += ACCEL[i] * delta;
-      
-      POSITION[i] += VELOCITY[i] * delta;
-      ORIENTATION[i] += GYRO[i] * delta;
+  } else if ((mpuIntStatus & (0x01 << MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
+    INFO("FIFO overflow! Resetting...");
+    mpu.resetFIFO();
+  } else if (mpuIntStatus & (0x01 << MPU6050_INTERRUPT_DMP_INT_BIT)) {
+    while (fifoCount >= packetSize) {
+      mpu.getFIFOBytes(fifoBuffer, packetSize);
+      fifoCount -= packetSize;
     }
-  }
 
-#endif
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetAccel(&aa, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+    mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+
+    #ifdef TEAPOT_ENABLED
+
+      #define mapBufferToTeapot(tp, fb) teapotPacket[tp] = fifoBuffer[fb];
+
+      mapBufferToTeapot(2, 0);
+      mapBufferToTeapot(3, 1);
+      mapBufferToTeapot(4, 4);
+      mapBufferToTeapot(5, 5);
+      mapBufferToTeapot(6, 8);
+      mapBufferToTeapot(7, 9);
+      mapBufferToTeapot(8, 12);
+      mapBufferToTeapot(9, 13);
+
+      Serial.write(teapotPacket, 14); // flushes teapotPacket to Serial.
+
+      teapotPacket[11]++; // teapot packetCount (overflow loop at 0xFF is intended).
+
+    #endif
+  }
+}
+  
 
 #ifdef USE_WIFI_MQTT
 
